f54891b1359acc16fde35efdbbe27637c31152bd
Alex Martinez <alexkmartinez77@gmail.com>
2022-08-01 17:22:21 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const profile = require("./profile");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/profile", profile);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.post("/thanks", (req, res) => {
+  res.render("thanks", { contact: req.body });
+});
+
+app.listen(8080, () => console.log("Server listening at http:/localhost:8080"));
